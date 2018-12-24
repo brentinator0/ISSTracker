986f42fbd99b8dc506de1ad60be006fa50cf3d7c
Brent Piephoff <bpiephoff@aligntech.com>
2018-12-23 23:05:16 -0500

    Creates Xcode Project

---
+++ ISSTracker/ISSTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ISSTracker
+//
+//  Created by Brent Piephoff on 12/23/18.
+//  Copyright © 2018 Brent P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
